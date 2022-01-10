a928319dfaf94df1f0b31f630aee4db308d4dd2a
FelipeBrigagao <38021142+FelipeBrigagao@users.noreply.github.com>
2022-01-10 18:53:06 -0300

    Started the project

---
+++ Assets/_Scripts/Player/Cannon/CannonInput.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonInput : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Unity Methods
+    #endregion
+
+    #region Methods
+    #endregion
+
+}
---
+++ Assets/_Scripts/Player/Colector/ColectorInput.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColectorInput : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Unity Methods
+    #endregion
+
+    #region Methods
+    #endregion
+
+}
